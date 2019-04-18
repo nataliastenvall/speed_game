b7f6d7e723c118ac09f3e8e451d83ea0c3da4c25
nataliastenvall <47786456+nataliastenvall@users.noreply.github.com>
2019-04-18 13:47:49 +0300

    New components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import Header from './Header/Header'
+//import Footer from './Footer/Footer'
+//import Overlay from './Overlay/Overlay'
+import Round from './Round/Round'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="allrounds">
+        <Round />
+        <Round />
+        <Round />
+        <Round />
+        </div>
       </div>
     );
   }
---
+++ src/Round/Round.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Round.css';
+
+class Round extends Component {
+  render() {
+    return (
+      <div className="round" id="button0"></div>
+    );
+  }
+}
+
+export default Round;
